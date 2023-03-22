40ec2cb9a9e6d03772937fb9a59bdcf1ddbd7cbc
Oryto Mikli <orytomikli21@gmail.com>
2023-03-22 18:30:00 +0700

    add scrollbar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,10 +3,10 @@ import NavigationBar from '../components/NavigationBar';
 
 export default function HomePage() {
   return (
-    <div className="w-full bg-sky-400  bg-cover h-[100vh]">
+    <div className="w-full bg-sky-400  bg-cover min-h-full py-4  ">
       <NavigationBar />
       <div className="flex items-center justify-center">
-        <div className="w-[80%] h-[90vh] text-white backdrop-blur-sm rounded-3xl">Hallo</div>
+        <div className="w-[90vw] h-[90vh] text-white backdrop-blur-sm rounded-3xl "></div>
       </div>
     </div>
   );
