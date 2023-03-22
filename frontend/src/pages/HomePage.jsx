88fe1078d271f67c6a7bcb9e88363fd89a78c229
orymikoto <rosbergroll99@gmail.com>
2023-03-22 18:30:00 +0700

    add leaderboard to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LeaderboardBar from '../components/LeaderboardBar';
 import NavigationBar from '../components/NavigationBar';
 
 export default function HomePage() {
@@ -6,7 +7,10 @@ export default function HomePage() {
     <div className="w-full bg-sky-400  bg-cover min-h-full py-4  ">
       <NavigationBar />
       <div className="flex items-center justify-center">
-        <div className="w-[90vw] h-[90vh] text-white backdrop-blur-sm rounded-3xl "></div>
+        <div className="w-[90vw] h-[90vh] text-white rounded-3xl flex ">
+          <LeaderboardBar />
+          <div></div>
+        </div>
       </div>
     </div>
   );
