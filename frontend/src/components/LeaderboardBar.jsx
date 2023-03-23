d24b181650f2f6c8439857c05cb0d1c10351dedd
orymikoto <rosbergroll99@gmail.com>
2023-03-23 18:30:00 +0700

    leaderboard functionality

--- frontend/src/components/LeaderboardBar.jsx
+++ frontend/src/components/LeaderboardBar.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function LeaderboardBar() {
-  return <div>LeaderboardBar</div>;
+export default function LeaderboardBar({ list = [{ name: '', score: 0 }] }) {
+  return (
+    <div className="bg-white shadow-md flex flex-col rounded-lg p-4 gap-y-2">
+      {list.map((e, i) => (
+        <div
+          className={`flex items-center h-8 text-neutral-900 text-lg gap-x-2 px-2 rounded-md ${
+            i % 2 == 0 ? 'bg-neutral-200' : 'bg-neutral-100'
+          } `}>
+          <p className="font-bold text-amber-400 ">{i + 1}</p>
+          <p className=" font-light text-neutral-600 w-24">{e.name}</p>
+          <p className="">{e.score}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,8 +7,17 @@ export default function HomePage() {
     <div className="w-full bg-sky-400  bg-cover min-h-full py-4  ">
       <NavigationBar />
       <div className="flex items-center justify-center">
-        <div className="w-[90vw] h-[90vh] text-white rounded-3xl flex ">
-          <LeaderboardBar />
+        <div className="w-[90vw] h-[90vh] text-white rounded-3xl flex items-start ">
+          <LeaderboardBar
+            list={[
+              { name: 'Mikoto', score: 240 },
+              { name: 'Mikail', score: 210 },
+              { name: 'Mikoto2', score: 190 },
+              { name: 'Mikail2', score: 180 },
+              { name: 'Mikoto3', score: 155 },
+              { name: 'Mikail3', score: 135 }
+            ]}
+          />
           <div></div>
         </div>
       </div>
