1b59bd9d2348f631dae5250ec76c5ca63300f0b3
orymikoto <rosbergroll99@gmail.com>
2023-04-01 18:30:00 +0700

    add api

--- backend/app/Http/Controllers/QuizController.php
+++ backend/app/Http/Controllers/QuizController.php
@@ -12,7 +12,10 @@ class QuizController extends Controller
      */
     public function index()
     {
-        
+        $quiz = Quiz::inRandomOrder()->limit(30)->get();
+        return response()->json([
+            'data' => $quiz
+        ]) ;
     }
 
     /**
@@ -20,7 +23,7 @@ class QuizController extends Controller
      */
     public function create()
     {
-        
+        // 
     }
 
     /**
@@ -28,7 +31,42 @@ class QuizController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $quiz = Quiz::create([
+            'question' => $request->question,
+            'a_answer' => $request->a_answer,
+            'a_correct' => $request->a_correct,
+            'b_answer' => $request->b_answer,
+            'b_correct' => $request->b_correct
+        ]);
+
+        return response()->json([
+            'message' => 'Data Successfully Stored',
+            'data' => $quiz
+        ]);
+        
+    }
+
+    public function store_many(Request $request){
+        $data = $request->all();
+        $final_array = array();
+
+
+        foreach ($data as $key => $value) {
+            array_push($final_array, array(
+                'question' => $value['question'],
+                'a_answer' => $value['a']['answer'],
+                'a_correct' => $value['a']['correct'],
+                'b_answer' => $value['b']['answer'],
+                'b_correct' => $value['b']['correct'],
+            ));
+            # code...
+        }
+        $quizzes = Quiz::insert($final_array);
+
+        return response()->json([
+            'message' => 'all data is inserted',
+            'data' => $quizzes,
+        ]);
     }
 
     /**
@@ -52,7 +90,18 @@ class QuizController extends Controller
      */
     public function update(Request $request, Quiz $quiz)
     {
-        //
+        $quiz->id = $request->id;
+        $quiz->question = $request->question;
+        $quiz->a_answer = $request->a_answer;
+        $quiz->a_correct = $request->a_correct;
+        $quiz->b_answer = $request->b_answer;
+        $quiz->b_correct = $request->b_correct;
+        $quiz->save();
+
+        return response()->json([
+            'message' => 'Data Updated Successfully',
+            'data' => $quiz
+        ]);
     }
 
     /**
@@ -60,6 +109,9 @@ class QuizController extends Controller
      */
     public function destroy(Quiz $quiz)
     {
-        //
+        $quiz->delete();
+        return response()->json([
+            'message' => 'Quiz Deleted'
+        ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,5 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/quiz/many', [QuizController::class, 'store_many' ]);
 
 Route::apiResource('quiz', QuizController::class );
